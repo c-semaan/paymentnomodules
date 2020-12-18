4fac3adccf2aab69742ea9afbf13627f4fd38dae
c-semaan <73475621+c-semaan@users.noreply.github.com>
2020-12-19 02:11:30 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command (if executed) installs the "cross-env"'
-echo 'dependency into the local "node_modules" directory, which will ultimately'
-echo 'be stored in the Jenkins home directory. As described in'
-echo 'https://docs.npmjs.com/cli/install, the "--save-dev" flag causes the'
-echo '"cross-env" dependency to be installed as "devDependencies". For the'
-echo 'purposes of this tutorial, this flag is not important. However, when'
-echo 'installing this dependency, it would typically be done so using this'
-echo 'flag. For a comprehensive explanation about "devDependencies", see'
-echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
+echo 'The following "npm" command builds your Node.js/React application for'
+echo 'production in the local "build" directory (i.e. within the'
+echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
+echo 'correctly bundles React in production mode and optimizes the build for'
+echo 'the best performance.'
 set -x
-# npm install --save-dev cross-env
+npm run build
 set +x
 
-echo 'The following "npm" command tests that your simple Node.js/React'
-echo 'application renders satisfactorily. This command actually invokes the test'
-echo 'runner Jest (https://facebook.github.io/jest/).'
+echo 'The following "npm" command runs your Node.js/React application in'
+echo 'development mode and makes the application available for web browsing.'
+echo 'The "npm start" command has a trailing ampersand so that the command runs'
+echo 'as a background process (i.e. asynchronously). Otherwise, this command'
+echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
+echo 'is followed by another command that retrieves the process ID (PID) value'
+echo 'of the previously run process (i.e. "npm start") and writes this value to'
+echo 'the file ".pidfile".'
 set -x
-npm test
+npm start &
+sleep 1
+echo $! > .pidfile
+set +x
+
+echo 'Now...'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
+echo 'created your initial Pipeline as a Jenkinsfile.)'
