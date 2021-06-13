74d1a1f0d53d7f9aeb72c3d828447fc48cb1a4e3
c-semaan <73475621+c-semaan@users.noreply.github.com>
2021-06-13 20:22:36 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,23 @@
+import React from 'react' ;
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+it("Renders without crashing", ()=> {
+  const div = document.createElement("div");
+  ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+  reportWebVitals();
+
+})
+   
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
