2dc446aa72a2336732db64d801697e15f23f616e
c-semaan <73475621+c-semaan@users.noreply.github.com>
2021-06-17 13:32:32 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -23,6 +23,10 @@ sleep 1
 echo $! > .pidfile
 set +x
 
+set -x
+npm test
+set +x
+
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
